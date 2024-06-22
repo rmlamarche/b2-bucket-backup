134a6b22c9a23b93afac0e9ee0fe7588a63f79f9
kubeuser <kubeuser@kube01.rmlamarche.com>
2024-06-22 15:06:57 +0000

    add mealie, restructure actions, this will probably not work but there's only 1 way to find out

---
+++ src/mealie/backup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import os
+import requests
+import subprocess
+
+def main():
+    API_TOKEN = os.environ.get('MEALIE_API_TOKEN')
+    BASE_URL = os.environ.get('MEALIE_BASE_URL')
+    B2_APPLICATION_KEY_ID = os.environ.get('B2_APPLICATION_KEY_ID')
+    B2_APPLICATION_KEY = os.environ.get('B2_APPLICATION_KEY')
+    B2_BUCKET = os.environ.get('B2_BUCKET')
+
+    auth_headers = {
+        "Authorization": "Bearer {}".format(API_TOKEN)
+    }
+
+    r = requests.get("{}{}".format(BASE_URL, "/api/admin/backups"), headers=auth_headers)
+    print(r.json())
+
+    r = requests.post("{}{}".format(BASE_URL, "/api/admin/backups"), headers=auth_headers)
+    print(r.json())
+
+    r = requests.get("{}{}".format(BASE_URL, "/api/admin/backups"), headers=auth_headers)
+    backup_to_download = r.json()['imports'][0]['name']
+
+    print('Downloading backup: {}'.format(backup_to_download))
+    r = requests.get("{}{}".format(BASE_URL, "/api/admin/backups/{}".format(backup_to_download)), headers=auth_headers)
+    fileToken = r.json()['fileToken']
+    tmp_file_path = '/tmp/{}.zip'.format(backup_to_download)
+    with requests.get("{}{}".format(BASE_URL, "/api/utils/download?token={}".format(fileToken)), headers=auth_headers, stream=True) as r:
+        r.raise_for_status()
+        with open(tmp_file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    print('Wrote backup to tmp file, uploading to b2')
+    cmd = ['b2', 'account', 'get']
+    subprocess.run(cmd)
+    cmd = ['b2', 'file', 'upload', B2_BUCKET, tmp_file_path, backup_to_download, '--no-progress']
+    subprocess.run(cmd)
+
+if __name__ == '__main__':
+    main()
