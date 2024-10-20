658a351a3d5df6e96d92d3be5b3e638d90cd464c
rmlamarche <ry.lamarche@gmail.com>
2024-10-20 14:22:06 -0400

    generic entrypoint

---
+++ src/common/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# start.sh: wrapper script that sends success/failure notifications via ntfy based on exit code
+
+# helper function to send ntfy push notification on success
+function send_ntfy_success {
+    /app/common/ntfy.py -u $NTFY_URL -T $NTFY_SUCCESS_TITLE -m $NTFY_SUCCESS_MESSAGE -p $NTFY_SUCCESS_PRIORITY
+}
+# helper function to send ntfy push notification on failure
+function send_ntfy_failure {
+    /app/common/ntfy.py -u $NTFY_URL -T $NTFY_FAILURE_TITLE -m $NTFY_FAILURE_MESSAGE -p $NTFY_FAILURE_PRIORITY
+}
+
+# Run supplied backup script
+$1
+
+# send push notifications based on exit code
+if [ $? -ne 0 ]; then
+    if [ -z "${NTFY_URL}" ]; then
+        echo "ERROR: backup failed, no NTFY_URL set. Set NTFY_URL=ntfy.sh/my-topic to receive alerts."
+        exit 1
+    fi
+    send_ntfy_failure
+else
+    if [ -z "${NTFY_URL}" ]; then
+        exit 0
+    fi
+    send_ntfy_success
+fi
