2c3ca48d1a5a0c999bd4cf3bdc5958470a269b6c
rmlamarche <ry.lamarche@gmail.com>
2024-10-20 14:54:07 -0400

    time to update them all hopefully this doesnt break all my backups but at least if it does i will get notifs probably

--- src/common/start.sh
+++ src/common/start.sh
@@ -3,26 +3,32 @@
 
 # helper function to send ntfy push notification on success
 function send_ntfy_success {
-    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_SUCCESS_TITLE}" -m "${NTFY_SUCCESS_MESSAGE}" -p "${NTFY_SUCCESS_PRIORITY}"
+    local time_elapsed="${1}"
+    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_SUCCESS_TITLE}" -m "${NTFY_SUCCESS_MESSAGE} $time_elapsed" -p "${NTFY_SUCCESS_PRIORITY}"
 }
 # helper function to send ntfy push notification on failure
 function send_ntfy_failure {
-    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_FAILURE_TITLE}" -m "${NTFY_FAILURE_MESSAGE}" -p "${NTFY_FAILURE_PRIORITY}"
+    local time_elapsed="${1}"
+    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_FAILURE_TITLE}" -m "${NTFY_FAILURE_MESSAGE} $time_elapsed" -p "${NTFY_FAILURE_PRIORITY}"
 }
 
 # Run supplied backup script
+SECONDS=0
 $1
+RESULT=$?
+DURATION=$SECONDS
+TIME_ELAPSED="(took $((DURATION / 60))m $((DURATION % 60))s)"
 
 # send push notifications based on exit code
-if [ $? -ne 0 ]; then
+if [ $RESULT -ne 0 ]; then
     if [ -z "${NTFY_URL}" ]; then
-        echo "ERROR: backup failed, no NTFY_URL set. Set NTFY_URL=ntfy.sh/my-topic to receive alerts."
+        echo "ERROR: backup failed, no NTFY_URL set. Set NTFY_URL=https://ntfy.sh/my-topic to receive alerts."
         exit 1
     fi
-    send_ntfy_failure
+    send_ntfy_failure $TIME_ELAPSED
 else
     if [ -z "${NTFY_URL}" ]; then
         exit 0
     fi
-    send_ntfy_success
+    send_ntfy_success $TIME_ELAPSED
 fi
