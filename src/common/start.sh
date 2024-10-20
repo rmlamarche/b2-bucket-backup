6ac42e77fb79ad4092dba07e61201f3d428d89b1
rmlamarche <ry.lamarche@gmail.com>
2024-10-20 15:09:10 -0400

    duration as message

--- src/common/start.sh
+++ src/common/start.sh
@@ -4,12 +4,12 @@
 # helper function to send ntfy push notification on success
 function send_ntfy_success {
     local time_elapsed="${1}"
-    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_SUCCESS_TITLE}" -m "${NTFY_SUCCESS_MESSAGE} $time_elapsed" -p "${NTFY_SUCCESS_PRIORITY}"
+    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_SUCCESS_TITLE}" -m "${time_elapsed}" -p "${NTFY_SUCCESS_PRIORITY}"
 }
 # helper function to send ntfy push notification on failure
 function send_ntfy_failure {
     local time_elapsed="${1}"
-    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_FAILURE_TITLE}" -m "${NTFY_FAILURE_MESSAGE} $time_elapsed" -p "${NTFY_FAILURE_PRIORITY}"
+    /bbb/common/ntfy.py -u "${NTFY_URL}" -T "${NTFY_FAILURE_TITLE}" -m "${time_elapsed}" -p "${NTFY_FAILURE_PRIORITY}"
 }
 
 # Run supplied backup script
@@ -17,7 +17,7 @@ SECONDS=0
 $1
 RESULT=$?
 DURATION=$SECONDS
-TIME_ELAPSED="(took $((DURATION / 60))m $((DURATION % 60))s)"
+TIME_ELAPSED="duration: $((DURATION / 60))m $((DURATION % 60))s"
 
 # send push notifications based on exit code
 if [ $RESULT -ne 0 ]; then
